1c902a616b85e4e0b8bdece119693e60773f8f87
Bhargav Sonagra <sonagrabhargav@gmail.com>
2025-08-01 14:10:43 +0530

    Update Footers.jsx

--- src/components/UI/Footers.jsx
+++ src/components/UI/Footers.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
-import { FaGithub, FaLinkedin } from 'react-icons/fa'; // Import icons
-
+import { FaGithub, FaLinkedin } from 'react-icons/fa'; 
 const Footers = () => {
   return (
     <footer>
       <div className="px-6 max-w-screen-xl mx-auto">
         <div className="fixed bottom-0 left-0 w-full bg-gray-800 text-white px-6 p-4 z-10">
+
           {/* Main content container */}
           <div className="flex flex-col md:flex-row md:justify-between md:items-center space-y-4 md:space-y-0">
             
             {/* Left Section */}
+
             <div className="text-center md:text-left">
               <h2 className="text-l font-bold text-blue-400">WorldAtlas</h2>
               <p className="text-sm text-gray-400 mt-1">© 2025 All Rights Reserved</p>
             </div>
 
             {/* Center Section */}
+
             <div className="text-center md:text-left">
               <h2 className="text-l font-bold text-blue-400">Created By</h2>
               <p className="text-sm text-gray-400 mt-1">Bhargav</p>
             </div>
 
             {/* Right Section (Icons) */}
+
             <div className="flex justify-center space-x-6">
               {/* LinkedIn Icon */}
               <a
@@ -34,6 +37,7 @@ const Footers = () => {
               </a>
 
               {/* GitHub Icon */}
+
               <a
                 href="https://github.com/BhargavSonagra"
                 target="_blank"
